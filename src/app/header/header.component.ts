1a36b32a0963feb93a3ea37958774137935afc76
Rova Fitiavana <Rova.Randrianarisoa@pulse.mg>
2023-02-01 15:38:38 +0300

    add icons @fortawesome/fontawesome-free - npm (npmjs.com)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { faPowerOff, faUser } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   faPowerOff = faPowerOff;
   faUser = faUser;
+  urlLogo: string;
+
+  constructor() {
+    this.urlLogo = "../assets/images/logo-test.png";
+  }
+  ngOnInit() {
+
+  }
 }
